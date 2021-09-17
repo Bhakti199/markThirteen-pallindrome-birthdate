5384da51247b73367062db45b939bc75650422de
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2021-09-17 22:45:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -46,11 +46,12 @@ function reverseStr(str) {
     var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
     var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
     var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
-    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
-    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
-    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+    // var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    // var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    // var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
   
-    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+    return [ddmmyyyy, mmddyyyy, yyyymmdd];
+      // , ddmmyy, mmddyy, yymmdd];
   }
   
   function checkPalindromeForAllDateFormats(date){
